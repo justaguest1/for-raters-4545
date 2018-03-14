b4bcf4aaf42d9669de99c1360a9feae2db08dc75
justaguest1 <37360279+justaguest1@users.noreply.github.com>
2018-03-14 18:20:34 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,2 @@
 <script>
-//$(function () {
-//            alert('Only Numerical data allowed');
-//    });
-            
-            
-var dostuff = function() {
-                       if( $("a").hasClass("button")){
-
-                       	alert('Button is Present');
-                       }
-                       else {alert('Button is not present');
-                       }
-                       
-                       };	
-            
 </script>
