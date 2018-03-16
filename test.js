29786780c946bbc2216961c95dc3017437986081
justaguest1 <37360279+justaguest1@users.noreply.github.com>
2018-03-16 12:21:55 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var changes = new Audio('https://raw.githubusercontent.com/justaguest1/for-raters-4545/master/changes.ogg');            
 $(document).ready(function () {
 function dostuff(){
-                       if( $("#task-index > div.container > ul > li > a").hasClass("button")){
+                       if( $("#task-index > div.container > ul > li:nth-child(1) > a").hasClass("button")){
                        	changes.play();
                        }
                        else {
