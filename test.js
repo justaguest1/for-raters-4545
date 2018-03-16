7f531d289119e91144b55225c420f426b5f88ea4
justaguest1 <37360279+justaguest1@users.noreply.github.com>
2018-03-16 13:06:51 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
+
 var changes = new Audio('https://raw.githubusercontent.com/justaguest1/for-raters-4545/master/changes.ogg');            
-$(document).ready(function () {
 function dostuff(){
                        if( $("#task-index > div.container > ul > li:nth-child(n) > a").hasClass("button")){
                        	changes.play();
@@ -10,4 +10,3 @@ function dostuff(){
                        }
                        }   
 setInterval(dostuff, 60000);
-  });           	
