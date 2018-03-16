1d205b7ca3cd435ab3a75b2ade02dda8223b9c73
justaguest1 <37360279+justaguest1@users.noreply.github.com>
2018-03-16 12:44:22 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 var changes = new Audio('https://raw.githubusercontent.com/justaguest1/for-raters-4545/master/changes.ogg');            
 $(document).ready(function () {
 function dostuff(){
-                       if( $("#task-index > div.container > ul > li:nth-child(1) > a").hasClass("button")){
+                       if( $("#task-index > div.container > ul > li:nth-child(n) > a").hasClass("button")){
                        	changes.play();
                        }
                        else {
-                       	 $( '#mainf' ).attr( 'src', function ( i, val ) { return val; });
+                         alert('button not there');
+                       	 //$( '#mainf' ).attr( 'src', function ( i, val ) { return val; });
                        }
                        }   
 setInterval(dostuff, 60000);
