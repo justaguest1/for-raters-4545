1b0f5700527a27c8b40565acb52649768967c1aa
justaguest1 <37360279+justaguest1@users.noreply.github.com>
2018-03-17 19:36:11 +0530

    Update tga.js

--- tga.js
+++ tga.js
@@ -1,22 +1,25 @@
 if (window.location.pathname == '/evaluation/rater'){ //For TGA
-var time = prompt('Time');
+var addradio = $('#task-index', window.parent.frames[0].document).append('<div id="popupPrompt"><input type="radio" name="refPage" id="ref" val="Yes" checked="true"/>Refresh<input type="radio" name="refPage" id="refAc" val="No"/>Refresh + Accept<br /></div>');
 var changes = new Audio('https://raw.githubusercontent.com/justaguest1/for-raters-4545/master/changes.ogg');            
 function dostuff(){
                        if( $('#task-index > div.container > ul > li:nth-child(n) > a', window.parent.frames[0].document).hasClass("button")){
                        	changes.play();
                        }
                        else {
-                       //$( '#mainf' ).attr( 'src', function ( i, val ) { return val; });
-                         alert("This page will reload");
+                       $( '#mainf' ).attr( 'src', function ( i, val ) { return val; });
+                       addradio();
                        }
                        }   
-setInterval(dostuff, time);
+setInterval(dostuff, 60000);
 };
+
+
+
 
 //To Remember The Choice of TGA
 $(function()
 {
-    $('input[type=radio]', window.parent.frames[0].document).each(function()
+    $('input[type=radio]').each(function()
     {
         var state = JSON.parse( localStorage.getItem('radio_'  + this.id) );
         
@@ -26,7 +29,7 @@ $(function()
 
 $(window).bind('unload', function()
 {
-    $('input[type=radio]', window.parent.frames[0].document).each(function()
+    $('input[type=radio]').each(function()
     {
         localStorage.setItem(
             'radio_' + this.id, JSON.stringify({checked: this.checked})
