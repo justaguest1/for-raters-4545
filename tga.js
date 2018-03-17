ee65e136d1b00796680126bab0ed90c16b8b6c24
justaguest1 <37360279+justaguest1@users.noreply.github.com>
2018-03-17 19:51:58 +0530

    Update tga.js

--- tga.js
+++ tga.js
@@ -1,13 +1,17 @@
 if (window.location.pathname == '/evaluation/rater'){ //For TGA
-$('#task-index', window.parent.frames[0].document).append('<div id="popupPrompt"><input type="radio" name="refPage" id="ref" val="Yes" checked="true"/>Refresh<input type="radio" name="refPage" id="refAc" val="No"/>Refresh + Accept<br /></div>');
+var addRadio = $('#task-index', window.parent.frames[0].document).append('<div id="popupPrompt"><input type="radio" name="refPage" id="ref" val="Yes" checked="true"/>Refresh<input type="radio" name="refPage" id="refAc" val="No"/>Refresh + Accept<br /></div>');
 var changes = new Audio('https://raw.githubusercontent.com/justaguest1/for-raters-4545/master/changes.ogg');            
+dalay(addRadio, 2000);
 function dostuff(){
+                       $('#task-index', window.parent.frames[0].document).append('<div id="popupPrompt"><input type="radio" name="refPage" id="ref" val="Yes" checked="true"/>Refresh<input type="radio" name="refPage" id="refAc" val="No"/>Refresh + Accept<br /></div>');
                        if( $('#task-index > div.container > ul > li:nth-child(n) > a', window.parent.frames[0].document).hasClass("button")){
                        	changes.play();
                        }
                        else {
                        $( '#mainf' ).attr( 'src', function ( i, val ) { return val; });
+$(document).ready(function() {                         
                        $('#task-index', window.parent.frames[0].document).append('<div id="popupPrompt"><input type="radio" name="refPage" id="ref" val="Yes" checked="true"/>Refresh<input type="radio" name="refPage" id="refAc" val="No"/>Refresh + Accept<br /></div>');
+ });
                        }
                        }   
 setInterval(dostuff, 60000);
