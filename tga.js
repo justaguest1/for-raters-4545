81d94ea2403c480c9fdfdf488a719988801f0bdd
justaguest1 <37360279+justaguest1@users.noreply.github.com>
2018-03-19 17:45:58 +0530

    Update tga.js

--- tga.js
+++ tga.js
@@ -1,21 +1,46 @@
 if (window.location.pathname == '/evaluation/rater'){ //For TGA
-var addRadio = $('#task-index', window.parent.frames[0].document).append('<div id="popupPrompt"><input type="radio" name="refPage" id="ref" val="Yes" checked="true"/>Refresh<input type="radio" name="refPage" id="refAc" val="No"/>Refresh + Accept<br /></div>');
+var addRadio = $('#task-index', window.parent.frames[0].document).append('<div id="popupPrompt"><input type="radio" name="refPage" id="ref" value="Yes" checked="true"/>Refresh<input type="radio" name="refPage" id="refAc" value="No"/>Refresh + Accept<br /></div>');
 var changes = new Audio('https://raw.githubusercontent.com/justaguest1/for-raters-4545/master/changes.ogg');            
+setTimeout(dostuff, 5000);
 function dostuff(){
-                       $('#task-index', window.parent.frames[0].document).append('<div id="popupPrompt"><input type="radio" name="refPage" id="ref" val="Yes" checked="true"/>Refresh<input type="radio" name="refPage" id="refAc" val="No"/>Refresh + Accept<br /></div>');
+                       $('#task-index', window.parent.frames[0].document).append('<div id="popupPrompt"><input type="radio" name="refPage" id="ref" value="Yes" checked="true"/>Refresh<input type="radio" name="refPage" id="refAc" value="No"/>Refresh + Accept<br /></div>');
                        if( $('#task-index > div.container > ul > li:nth-child(n) > a', window.parent.frames[0].document).hasClass("button")){
                        	changes.play();
                        }
                        else {
                        $( '#mainf' ).attr( 'src', function ( i, val ) { return val; });
-$(document).ready(function() {                         
-                       $('#task-index', window.parent.frames[0].document).append('<div id="popupPrompt"><input type="radio" name="refPage" id="ref" val="Yes" checked="true"/>Refresh<input type="radio" name="refPage" id="refAc" val="No"/>Refresh + Accept<br /></div>');
- });
+                       setTimeout(dostuff, 60000);
                        }
-                       }   
-setInterval(dostuff, 60000);
+                       }  
 };
 
+
+
+function dostuff2(){
+                       $('#task-index', window.parent.frames[0].document).append('<div id="popupPrompt"><input type="radio" name="refPage" id="ref" value="Yes" checked="true"/>Refresh<input type="radio" name="refPage" id="refAc" value="No"/>Refresh + Accept<br /></div>');
+                       if( $('#task-index > div.container > ul > li:nth-child(n) > a', window.parent.frames[0].document).hasClass("button")){
+                       	changes.play();
+                        $('#task-index > div.container > ul > li:nth-child(n) > a').click(); 
+                       }
+                       else {
+                       $( '#mainf' ).attr( 'src', function ( i, val ) { return val; });
+                       setTimeout(dostuff2, 60000);
+                       }
+                       }  
+};
+
+
+$('input:radio[name=refPage]').change(
+    function(){
+        if ($(this).val() == 'Yes') {
+dostuff();        }
+        else {
+
+if ($(this).val() == 'No') {
+dostuff2();
+}
+        }
+    });
 
 
 
